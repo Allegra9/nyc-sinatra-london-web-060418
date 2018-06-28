28b079d3091f07853b37dfdd0b693826d81a4776
Allegra <aukseallegra@gmail.com>
2018-06-28 16:56:40 +0100

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,25 +1,27 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
-    @figures = Figure.all
+    @figures = Figure.all    #index page, shows all
     erb :'/figures/index'
   end
 
   get '/figures/new' do
-    @titles = Title.all    #loads all of the titles from the database
-    @landmarks = Landmark.all   #all the landmarks
-    erb :'/figures/new'
+    erb :'/figures/new'   #form for a new figure
   end
 
-  post '/figures' do   #creates new figure w selected ...
-    binding.pry
-    figure = Figure.create(params[:figure])
-    title = Title.create(params[:title])
-    landmark = Landmark.create(params[:landmark])
-    figure.title_ids << title.id
-    figure.landmark_ids << landmark.id
+  # NEW:
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
 
-    redirect "figures/#{figure.id}"
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+    redirect "/figures/#{@figure.id}"
   end
 
   get '/figures/:id' do
@@ -29,19 +31,24 @@ class FiguresController < ApplicationController
 
   get '/figures/:id/edit' do
     @figure = Figure.find(params[:id])
-    @titles = Title.all    #loads all of the titles from the database
-    @landmarks = Landmark.all
     erb :"figures/edit"
   end
 
+  # EDIT:
   patch "/figures/:id" do
-    figure = Figure.find(params[:id])
-    figure.update(params[:figure])
-    title = Title.create(params[:title])
-    landmark = Landmark.create(params[:landmark])
-    figure.title_ids << title.id
-    figure.landmark_ids << landmark.id
-    redirect "/figures/#{figure.id}"
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    @figure.save
+    redirect "/figures/#{@figure.id}"
   end
 
   delete "/figures/:id" do
@@ -51,3 +58,77 @@ class FiguresController < ApplicationController
   end
 
 end
+
+
+# {
+#   "figure"=>{
+#     "name"=>"allegra",
+#     "title_ids"=>["1", "3", "4"],
+#     "landmark_ids"=>["4", "5", "7"]
+#   },
+#  "title"=>{
+#    "name"=>"the homegirl"
+#  },
+#  "landmark"=>{
+#    "name"=>"kings cross",
+#    "year_completed"=>"2018"
+#    }
+#  }
+
+# figure[name]
+# figure[title_ids][]
+# figure[landmark_ids][]
+
+# title[name]
+
+# landmark[name]
+# landmark[year_completed]
+
+# {
+#   "figure"=>{
+#     "name"=>"Barack Obama",
+#     "title_ids"=>["3"],
+#     "titles"=>{"name"=>"The First Black President"},
+#     "landmark_ids"=>["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+#     "landmarks"=>{"name"=>"The White House", "year_completed"=>"2015"}
+#     }
+#   }
+
+#
+# @figure = Figure.create(params[:figure])
+#
+# @figure.name = params[:figure][:name]
+# @figure.title_ids << params[:figure][:title_ids]   # << title.id
+# @figure.titles.name = params[:figure][:titles][:name]  "The First Black President"
+# @figure.landmark_ids << params[:figure][:landmark_ids] # "1"
+# @figure.landmarks.name = params[:figure][:landmarks][:name]  # "The White House"
+# @figure.landmarks.year_completed = params[:figure][:landmarks][:year_completed] # "2015"
+
+# if !params[:figure][:landmarks][:name].empty?
+#   @figure.landmarks << Landmark.create(params[:figure][:landmarks][:name])
+# end
+#
+# if !params[:figure][:titles][:name].empty?
+#   @figure.titles << Title.create(params[:figure][:titles][:name])
+# end
+
+# {
+#   "figure"=>{
+#     "name"=>"Barack Obama",
+#     "title_ids"=>["3"],
+#     "titles"=>{
+#         "name"=>"The First Black President"
+#     },
+#     "landmark_ids"=>["1"],
+#     "landmarks"=>{
+#         "name"=>"The White House",
+#         "year_completed"=>"2016"
+#     }
+#   }
+# }
+# figure[name]
+# figure[title_ids][]
+# figure[titles][name]
+# figure[landmark_ids][]
+# figure[landmarks][name]
+# figure[landmarks][year_completed]
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,48 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all    #index page, shows all
+    @figures = Figure.all    # !!!!
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'   #form for a new landmark
+  end
+
+  # NEW:
+  post "/landmarks" do
+    @landmark = Landmark.create
+    @landmark.name = params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed]
+    redirect '/landmarks'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  # EDIT:
+  patch "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+  #updating params:
+    @landmark.name = params["landmark"]["name"]
+    @landmark.year_completed = params["landmark"]["year_completed"]
+  #savind:
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  delete "/figures/:id" do
+    @figure = Figure.find(params[:id])
+    @figure.destroy
+    redirect "/figures"
+  end
 end
